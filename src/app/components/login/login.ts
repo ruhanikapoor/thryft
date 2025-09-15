7e31557c507a3e1835d336e7c1ea828c3d747438
ruhanikapoor <ruhanikapoor04@gmail.com>
2025-09-15 13:55:51 +0530

    feat: add user interface

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,9 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserInterface } from '../../../interfaces/user.interface';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
@@ -11,12 +13,25 @@ export class Login {
 
   router = inject(Router)
 
+  loginForm = new FormGroup<{
+    email: FormControl<string | null>;
+    password: FormControl<string | null>;
+  }>({
+    email: new FormControl('', { nonNullable: true, validators: [Validators.required, Validators.email] }),
+    password: new FormControl('', {nonNullable: true, validators: [Validators.required, Validators.minLength(6)]})
+  });
+
   signUpPage(){
     this.router.navigateByUrl("sign-up");
   }
 
   login() {
-    this.router.navigateByUrl("home");
+    if(this.loginForm.valid){
+      const user: UserInterface = this.loginForm.value as UserInterface;
+      this.router.navigateByUrl("home");
+    } else {
+      alert("Invalid form. Check credentials again");
+    }
   }
 
 }
--- src/app/components/register/register.ts
+++ src/app/components/register/register.ts
@@ -1,9 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserInterface } from '../../../interfaces/user.interface';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
@@ -11,12 +13,27 @@ export class Register {
 
   router = inject(Router);
 
+  signUpForm = new FormGroup<{
+    username: FormControl<string | null>;
+    email: FormControl<string | null>;
+    password: FormControl<string | null>;
+  }>({
+    username: new FormControl('', {nonNullable: true, validators: [Validators.required, Validators.minLength(3)]}),
+    email: new FormControl('', {nonNullable: true, validators: [Validators.required, Validators.email]}),
+    password: new FormControl('', {nonNullable: true, validators: [Validators.required, Validators.minLength(6)]})
+  });
+
   loginPage() {
     this.router.navigateByUrl("login");
   }
 
   signUp() {
-    this.router.navigateByUrl("home");
+    if(this.signUpForm.valid){
+      const user: UserInterface = this.signUpForm.value as UserInterface;
+      this.router.navigateByUrl("home");
+    } else {
+      alert("Invalid form. Check credentials again");
+    }
   }
 
 }
