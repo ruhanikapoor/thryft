c1a8715422021b2a5944577eed38dafcdb981e23
ruhanikapoor <ruhanikapoor04@gmail.com>
2025-09-16 14:15:35 +0530

    feat: add homepage layout

--- src/app/components/homepage/homepage.ts
+++ src/app/components/homepage/homepage.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Group } from '../../ui/group/group';
 
 @Component({
   selector: 'app-homepage',
-  imports: [],
+  imports: [Group],
   templateUrl: './homepage.html',
   styleUrl: './homepage.css'
 })
---
+++ src/app/ui/group/group.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Card } from '../card/card';
+
+@Component({
+  selector: 'app-group',
+  imports: [Card],
+  templateUrl: './group.html',
+  styleUrl: './group.css'
+})
+export class Group {
+
+}
