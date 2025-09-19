63cf7cadcb2032371ede6faba5b9d31083f6f2a1
ruhanikapoor <ruhanikapoor04@gmail.com>
2025-09-19 10:03:53 +0530

    fix: navbar bg

--- src/app/ui/navbar/navbar.ts
+++ src/app/ui/navbar/navbar.ts
@@ -1,6 +1,7 @@
 import { Component, HostListener, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -12,11 +13,31 @@ export class Navbar {
 
   isScrolled: boolean = false;
   menuVisible: boolean = false;
+  hasHeroSection: boolean = true;
   router = inject(Router);
+
+  constructor() {
+    this.router.events.pipe(filter(e => e instanceof NavigationEnd)).subscribe((e: any) => {
+      const currentUrl = e.urlAfterRedirects;
+
+      this.hasHeroSection = !(
+        currentUrl.includes('/product')
+      )
+
+      if(!this.hasHeroSection) {
+        this.isScrolled = true;
+      } else {
+        this.isScrolled = window.scrollY > window.innerHeight * 0.93;
+      }
+
+    })
+  }
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
-    this.isScrolled = window.scrollY > window.innerHeight * 0.93;
+    if(this.hasHeroSection){
+      this.isScrolled = window.scrollY > window.innerHeight * 0.93;
+    }
   }
 
   openMenu() {
