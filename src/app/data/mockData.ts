7ca8790653bdbe19f43b739d04213ccaa3443aed
ruhanikapoor <ruhanikapoor04@gmail.com>
2025-09-26 12:00:19 +0530

    feat: add category data

--- src/app/data/mockData.ts
+++ src/app/data/mockData.ts
@@ -2,17 +2,106 @@ import { Category } from "../../interfaces/category.interface";
 import { Listing } from "../../interfaces/listing.interface";
 
 export const MOCK_CATEGORIES: Category[] = [
-  { id: '1', name: 'Electronics', icon: '/assets/laptop.png' },
-  { id: '2', name: 'Fashion', icon: '/assets/fashion.png' },
-  { id: '3', name: 'Home', icon: '/assets/home-decor.png' },
-  { id: '4', name: 'Books', icon: '/assets/book1.png' },
-  { id: '5', name: 'Electronics', icon: '/assets/laptop.png' },
-  { id: '6', name: 'Fashion', icon: '/assets/fashion.png' },
-  { id: '7', name: 'Home', icon: '/assets/home-decor.png' },
-  { id: '8', name: 'Electronics', icon: '/assets/laptop.png' },
-  { id: '9', name: 'Fashion', icon: '/assets/fashion.png' },
-  { id: '10', name: 'Home', icon: '/assets/home-decor.png' },
+  {
+    id: '1',
+    name: 'Electronics',
+    icon: '/assets/plug.svg',
+    subCategories: [
+      { id: '1-1', name: 'Phones', icon: '/assets/plug.svg' },
+      { id: '1-2', name: 'Laptops', icon: '/assets/plug.svg' },
+      { id: '1-3', name: 'Cameras & Accessories', icon: '/assets/plug.svg' },
+      { id: '1-4', name: 'Gaming Consoles & Games', icon: '/assets/plug.svg' },
+      { id: '1-5', name: 'Headphones & Audio Gear', icon: '/assets/plug.svg' },
+      { id: '1-6', name: 'Small Appliances', icon: '/assets/plug.svg' },
+    ],
+  },
+  {
+    id: '2',
+    name: 'Fashion',
+    icon: '/assets/sunglasses.svg',
+    subCategories: [
+      { id: '2-1', name: 'Women\'s Clothing', icon: '/assets/plug.svg' },
+      { id: '2-2', name: 'Men\'s Clothing', icon: '/assets/plug.svg' },
+      { id: '2-3', name: 'Kids & Baby Clothing', icon: '/assets/plug.svg' },
+      { id: '2-4', name: 'Footwear', icon: '/assets/plug.svg' },
+      { id: '2-5', name: 'Bags & Backpacks', icon: '/assets/plug.svg' },
+      { id: '2-6', name: 'Jewelry & Watches', icon: '/assets/plug.svg' },
+      { id: '2-7', name: 'Hats, Scarves & Belts', icon: '/assets/plug.svg' },
+    ],
+  },
+  {
+    id: '3',
+    name: 'Home',
+    icon: '/assets/home-decor.svg',
+    subCategories: [
+      { id: '3-1', name: 'Furniture', icon: '/assets/plug.svg' },
+      { id: '3-2', name: 'Home Décor', icon: '/assets/plug.svg' },
+      { id: '3-3', name: 'Kitchen & Dining', icon: '/assets/plug.svg' },
+      { id: '3-4', name: 'Bedding & Textiles', icon: '/assets/plug.svg' },
+      { id: '3-5', name: 'Storage & Organization', icon: '/assets/plug.svg' },
+      { id: '3-6', name: 'Plants & Garden', icon: '/assets/plug.svg' },
+    ],
+  },
+  {
+    id: '4',
+    name: 'Books',
+    icon: '/assets/book.svg',
+    subCategories: [
+      { id: '4-1', name: 'Fiction & Non-Fiction', icon: '/assets/plug.svg' },
+      { id: '4-2', name: 'Textbooks & Academic', icon: '/assets/plug.svg' },
+      { id: '4-3', name: 'Comics & Graphic Novels', icon: '/assets/plug.svg' },
+      { id: '4-4', name: 'Magazines', icon: '/assets/plug.svg' },
+      { id: '4-5', name: 'Music (Vinyl, CDs, Cassettes)', icon: '/assets/plug.svg' },
+      { id: '4-6', name: 'Movies & DVDs', icon: '/assets/plug.svg' },
+    ],
+  },
+  {
+    id: '5',
+    name: 'Hobbies & Collectibles',
+    icon: '/assets/plant.svg',
+    subCategories: [
+      { id: '5-1', name: 'Art Supplies', icon: '/assets/plug.svg' },
+      { id: '5-2', name: 'Musical Instruments', icon: '/assets/plug.svg' },
+      { id: '5-3', name: 'Board Games & Puzzles', icon: '/assets/plug.svg' },
+      { id: '5-4', name: 'Collectibles', icon: '/assets/plug.svg' },
+      { id: '5-5', name: 'Sports & Fitness Gear', icon: '/assets/plug.svg' },
+      { id: '5-6', name: 'Handmade & DIY Kits', icon: '/assets/plug.svg' },
+    ],
+  },
+  {
+    id: '6',
+    name: 'Vehicles & Parts',
+    icon: '/assets/car.svg',
+    subCategories: [
+      { id: '6-1', name: 'Bicycles', icon: '/assets/plug.svg' },
+      { id: '6-2', name: 'Motorbikes', icon: '/assets/plug.svg' },
+      { id: '6-3', name: 'Car Parts & Accessories', icon: '/assets/plug.svg' },
+      { id: '6-4', name: 'Skateboards & Scooters', icon: '/assets/plug.svg' },
+    ],
+  },
+  {
+    id: '7',
+    name: 'Kids & Baby',
+    icon: '/assets/baby.svg',
+    subCategories: [
+      { id: '7-1', name: 'Toys & Games', icon: '/assets/plug.svg' },
+      { id: '7-2', name: 'Baby Gear', icon: '/assets/plug.svg' },
+      { id: '7-3', name: 'Kids\' Furniture', icon: '/assets/plug.svg' },
+    ],
+  },
+  {
+    id: '8',
+    name: 'Luxury & Vintage Finds',
+    icon: '/assets/vintage.svg',
+    subCategories: [
+      { id: '8-1', name: 'Designer Clothing', icon: '/assets/plug.svg' },
+      { id: '8-2', name: 'Rare Sneakers', icon: '/assets/plug.svg' },
+      { id: '8-3', name: 'Luxury Bags & Watches', icon: '/assets/plug.svg' },
+      { id: '8-4', name: 'Antiques', icon: '/assets/plug.svg' },
+    ],
+  },
 ];
+
 
 export const MOCK_LISTINGS: Listing[] = [
   {
--- src/interfaces/category.interface.ts
+++ src/interfaces/category.interface.ts
@@ -1,5 +1,12 @@
+export interface SubCategory {
+  id: string;
+  name: string;
+  icon: string;
+}
+
 export interface Category {
   id: string;
   name: string;
   icon: string;
+  subCategories?: SubCategory[];
 }
